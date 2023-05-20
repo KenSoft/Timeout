e7df0ebaffe9fa364c489b3b46f0af6b6d060357
BuildTools <kken62@gmail.com>
2023-05-21 01:49:16 +0900

    Add Parameter Support

--- commands/timeoutwithlove.js
+++ commands/timeoutwithlove.js
@@ -3,8 +3,16 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('timeoutwithlove')
-		.setDescription('Go to Bed!'),
+		.setDescription('Go to Bed!')
+		.addIntegerOption(option =>
+		option.setName('time')
+			.setDescription('Time in minute until timeout')
+			.setRequired(true)
+			// Ensure the text will fit in an embed description, if the user chooses that option
+			.setMinValue(0)
+			.setMaxValue(1440)),
 	async execute(interaction) {
-		await interaction.reply('Reply!');
+		const time = interaction.options.getInteger('time')
+		await interaction.reply(`Reply! ${time}`);
 	},
 };
