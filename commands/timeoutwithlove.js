1ee78dfd616008e8c08d997505b8fef80b7a437b
Saintsitive <sirirat.adelyn@gmail.com>
2023-05-23 03:26:26 +0700

    feat: refactor config firebase

--- commands/timeoutwithlove.js
+++ commands/timeoutwithlove.js
@@ -6,17 +6,8 @@ const { SlashCommandBuilder, PermissionFlagsBits  } = require('discord.js');
 const { Client, GatewayIntentBits } = require('discord.js');
 const Discord = require("discord.js");
 require('dotenv').config()
+const { firebaseConfig, botToken } = require('../core/config')
 
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
 
 const client = new Discord.Client({
   intents: [
@@ -25,7 +16,7 @@ const client = new Discord.Client({
   ]
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login(botToken);
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
